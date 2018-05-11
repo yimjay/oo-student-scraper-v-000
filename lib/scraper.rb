3b60e7d5969c5345ec4f4e2ac6dcc34fab39ee38
Ji Yim <gsooyim@gmail.com>
2018-05-11 00:40:45 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,7 @@ class Scraper
         student[:linkedin] = link
       elsif link.include?("github")
         student[:github] = link
-      elsif link.include?("blog")
+      else link.include?("blog")
         student[:blog] = link
       end
     end
