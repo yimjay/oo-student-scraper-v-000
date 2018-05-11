05192fe503b9511c8909aa4932e034365faea48a
Ji Yim <gsooyim@gmail.com>
2018-05-11 00:17:09 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,6 +9,7 @@ class Scraper
     page.css(".student-card").collect do |student|
       students_list << {:name => student.css("h4.student-name").text, :location => student.css("p.student-location").text, :profile_url =>   student.css("a").attribute("href").text}
     end
+    students_list
   end
 
   def self.scrape_profile_page(profile_url)
