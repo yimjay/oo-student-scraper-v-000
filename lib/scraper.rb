4bd6cbdbbe6bfbfc2401328ca0805bc7c3413ef1
Ji Yim <gsooyim@gmail.com>
2018-05-11 00:15:23 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,10 @@ class Scraper
   def self.scrape_index_page(index_url)
     students_list = []
     page = Nokogiri::HTML(open(index_url))
-    page.css(".roster-cards-container .student-card").collect do |student|
+    page.css(".student-card").collect do |student|
+      student_name = 
+      
+      
       students_list << {:name => student.css("h4").text, :location => student.css(".student-location").text, :profile_url =>   student.css("a").attribute("href").text}
     end
   end
